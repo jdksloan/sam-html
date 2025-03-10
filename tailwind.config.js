6607d567e36376088a4cb4dfb6f676b2f9e13419
Jethro Sloan <jdksloan@gmail.com>
2025-03-10 16:15:34 +0100

    Made changes to the layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
+    backgroundImage: {
+      jumbotron: "url('./images/nordwood-themes-nDd3dIkkOLo-unsplash.jpg')",
+    },
     extend: {
       fontFamily: {
         sans: ['"Roboto-light"', 'sans-serif'],
